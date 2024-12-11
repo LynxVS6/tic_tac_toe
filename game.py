2f0fd5d806a6250d77117d8d4aef487d99a346f8
Рысьева Виктория <Viktoriariseva@ya.ru>
2024-12-12 01:12:16 +0500

    create TIC_TAC_TOE game

---
+++ game.py
@@ -0,0 +1,24 @@
+from gameparts import Board
+from gameparts.exceptions import FieldIndexError
+
+
+def main():
+    game = Board()
+    game.display()
+    row = int(input('Введите номер строки: '))
+
+    if row < 0 or row >= game.field_size:
+        raise FieldIndexError
+
+    column = int(input('Введите номер столбца: '))
+
+    if column < 0 or column >= game.field_size:
+        raise FieldIndexError
+
+    game.make_move(row, column, 'X')
+    print('Ход сделан!')
+    game.display()
+
+
+if __name__ == '__main__':
+    main()
